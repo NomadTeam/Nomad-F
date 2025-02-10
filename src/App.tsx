c48870026ba931aa0e3120eb1873d620d3063873
Lee gangho <rkdgh1198@gmail.com>
2025-02-10 21:37:58 +0900

    chore : 프로젝트 의존성 설치 #5 - 리액트 쿼리 + 데브툴 - 주스탄드 + 엑시오스 - 리액트 아이콘

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-const App = () => {
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { FiHome } from 'react-icons/fi';
+
+const Test = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold'>테스트</h1>
+      <FiHome />
     </div>
+  );
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      throwOnError: true,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1 className='text-3xl font-bold text-red-500'>테스트</h1>
+        <Test />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
