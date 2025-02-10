734f3b635f0232f1804bcdd90030d90cb4d783b8
Lee gangho <rkdgh1198@gmail.com>
2025-02-10 21:32:03 +0900

    chore : tailwind pretierr 설정 #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-red-500'>테스트입니다</h1>
-      <button className='rounded-md bg-blue-500 p-2 text-white'>버튼</button>
-      <div className='flex h-screen flex-col items-center justify-center'>
-        <div className='h-1/2 w-1/2 rounded-md bg-red-500'>테스트</div>
-        <div className='h-1/2 w-1/2 rounded-md bg-red-500'>테스트</div>
-        <div className='h-1/2 w-1/2 rounded-md bg-red-500'>
-          테스트입니다
-          <span>테스트</span>
-        </div>
-      </div>
+      <h1 className='text-3xl font-bold'>테스트</h1>
     </div>
   );
 };
